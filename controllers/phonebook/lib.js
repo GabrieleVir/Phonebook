5b50625a81d6b53699b451271940d13144d82dc2
Gabriele Virga <Gabriele.virga@tiao.world>
2020-08-17 22:28:01 +0200

    call api for search Phonebook done, defence for redos is needed but I don't know how to implement it without adding constraint on first and last name

--- controllers/phonebook/lib.js
+++ controllers/phonebook/lib.js
@@ -11,7 +11,12 @@ async function createPhonebook(req, res) {
     let regex = /^[+][\d]{2}[ ][\d]{2}[ ][\d]{6,}/g;
     if (!(phonenumber.match(regex))) {
         return res.status(400).json({
-            text: "Invalid phonennumber"
+            text: "Invalid phonenumber"
+        });
+    }
+    if (typeof first_name !== 'string' || typeof last_name !== 'string') {
+        return res.status(400).json({
+            text: "First_name and last_name must be strings"
         });
     }
     const phonebook = {
@@ -45,4 +50,28 @@ async function createPhonebook(req, res) {
     }
 }
 
+async function searchPhonebook(req, res) {
+    let phonebookTextSearch = req.query.search === undefined ? '' : req.query.search;
+    if (phonebookTextSearch.indexOf('+') !== -1) {
+        phonebookTextSearch = '\\' + phonebookTextSearch;
+    }
+    const regexForMongodb = new RegExp(phonebookTextSearch, 'i');
+    const findPhonebook = await Phonebook.find({
+        $or: [
+            { first_name: regexForMongodb },
+            { last_name: regexForMongodb },
+            { phonenumber: regexForMongodb },
+        ]
+    });
+    if (Array.isArray(findPhonebook) && findPhonebook.length === 0) {
+        return res.status(404).json({
+            text: 'No entry found',
+        })
+    }
+    return res.status(200).json({
+        phonebooks: findPhonebook,
+    });
+}
+
 exports.createPhonebook = createPhonebook;
+exports.searchPhonebook = searchPhonebook;
--- controllers/phonebookController.js
+++ controllers/phonebookController.js
@@ -2,4 +2,5 @@ const phonebook = require('./phonebook/lib.js');
 
 module.exports = function (app) {
     app.post('/create', phonebook.createPhonebook);
+    app.get('/search', phonebook.searchPhonebook);
 }
